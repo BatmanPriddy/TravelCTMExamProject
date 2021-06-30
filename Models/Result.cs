5adfbfb13466abcfb5e3f564b67f8775f8bb8844
BatmanPriddy <jason.r.priddy@gmail.com>
2021-06-30 11:38:26 -0500

    committing typo fix in Result.cs class file

--- Models/Result.cs
+++ Models/Result.cs
@@ -8,7 +8,7 @@
 	{
 		//Data is the result data, generic here so it can be whatever you want
 		//ErrorMessage is for any failures you might encounter, that the user/call can see to determine issue
-		//Success is a boolean of hey it work, or nope, houston we have a problem!
+		//Success is a boolean of hey it works, or nope, houston we have a problem!
 		public T Data { get; set; }
 		public string ErrorMessage { get; set; }
 		public bool Success { get; set; }
